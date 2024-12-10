90fdaf0f1f856fd3a04666f62d60857645a911ac
Mahdieh Maleki <mahdiehmaleki96@gmail.com>
2024-12-10 01:58:23 +0000

    completed react-components

---
+++ react-components/my-app/src/Description.tsx
@@ -0,0 +1,11 @@
+export function Description() {
+  return (
+    <p>
+      Astronomers today still use constellations to name stars and meteor
+      showers. There are a few different definitions of constellations, but many
+      people think of constellations as a group of stars. Often, it's a group of
+      stars that looks like a particular shape in the sky and has been given a
+      name.
+    </p>
+  );
+}
